8c16812b9152a4aa9579016cb440ecbce468e2b7
Noah Taggart <noah-taggart@hotmail.com>
2024-04-08 23:57:20 -0400

    new bottom nav design almost working, one small problem left. New fab icon working that navigates to search on click and music search on long click.

--- app/src/main/java/com/example/anidex/MainActivity.java
+++ app/src/main/java/com/example/anidex/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.anidex;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -15,6 +18,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.anidex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
 
     private ActivityMainBinding binding;
 
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        navView.setBackground(null);
+
 
         //disable click of 3rd menu item (blank)
         Menu menu = navView.getMenu();
@@ -34,12 +41,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_id, R.id.navigation_fav)
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_id, R.id.navigation_fav,R.id.nav_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.searchFragment);
+            }
+        });
+
+        binding.fab.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                navController.navigate(R.id.navigation_music);
+                binding.fab.setImageResource(R.drawable.music);
+                return true;
+            }
+        });
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(NavController controller, NavDestination destination, Bundle arguments) {
+                if (destination.getId() == R.id.navigation_music) {
+                    // When navigating to music fragment, set music icon
+                    binding.fab.setImageResource(R.drawable.music);
+                } else {
+                    // For other fragments, set the original icon
+                    binding.fab.setImageResource(R.drawable.baseline_saved_search_24);
+                }
+            }
+        });
+
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+
 
 }
